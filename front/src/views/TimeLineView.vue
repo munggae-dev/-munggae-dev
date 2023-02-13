bcc6efcab738eb44b53c23b99c92413365faef8e
Munggae <sey621@gmail.com>
2023-02-13 16:20:09 +0900

    timeline commit

---
+++ front/src/views/TimeLineView.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title>
+            <span>Timeline</span>
+          </v-card-title>
+          <v-card-subtitle>
+            <span>2014 ~ 2023</span>
+          </v-card-subtitle>
+          <TimeLineComp></TimeLineComp>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import TimeLineComp from "@/components/timeline/TimeLineComp.vue";
+export default {
+  name: "TimeLine",
+
+  components: {
+    TimeLineComp,
+  },
+};
+</script>
